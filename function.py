90a449640ae28089dbd201352446ff1b2afcb004
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-03 12:33:24 +0530

    new file

---
+++ function.py
@@ -0,0 +1,17 @@
+def hey():
+    print('Hello World')
+
+hey()
+
+def add(a,b):
+    print(a+b)
+
+add(3,3)
+
+def name(n,age):
+    print('Name is '+n+'and age is '+str(age))
+
+name('Hello','12')
+
+val='world'
+name(val,12)
