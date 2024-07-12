ca6f569dd3c4164b29677d053573538247e270fe
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-12 11:56:21 +0530

    scope _global and local

--- sample_scope.py
+++ sample_scope.py
@@ -2,17 +2,22 @@ def check_scope():
     def do_local():
         test='local test'
     def do_non_local():
+        nonlocal test
         test='non local test'
     def do_global():
+        global test #global variable
         test='global test'
 
     test='default'
     do_local()
     print('test value after do local:',test)
+    do_non_local()
+    print('test value after do non local:',test)
+    do_global()
+    print('test value after do global:',test)
 
-#fun do_local and other are local ,so they didnt call
-
-
+#func do_local and other are local ,so they didnt call
 
 
 check_scope()
+print('test value after do non local:',test)
