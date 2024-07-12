94bb321b96fc62f20fc845c1fc93e176d3dbb4dd
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-12 14:07:36 +0530

    class example

--- class_sample.py
+++ class_sample.py
@@ -1,7 +1,16 @@
 class MySampleClass:
-    def hello(self):
-        print('Hello World')
+    def hello(self,n):
+        self.name=n
+
+    def print_name(self):
+        print(self.name)
 
 
 x=MySampleClass()
-x.hello()
+namee='apple'
+x.hello(namee)
+x.print_name()
+
+y=MySampleClass()
+y.hello('orange')
+y.print_name()
