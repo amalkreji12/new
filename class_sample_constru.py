8ebdb6c4aa8f6ec3e08a0745c561c9fc3532ed59
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-13 15:21:27 +0530

    class example

--- class_sample_constru.py
+++ class_sample_constru.py
@@ -1,5 +1,5 @@
-class RacingTeamMenmers:
-    year=2024
+class RacingTeamMembers:
+    year=2024  #this year is for whole class
     def __init__(self,name,age,place):   #--> constructor
         self.name=name
         self.age=age
@@ -16,13 +16,31 @@ class RacingTeamMenmers:
         print('Name :',self.name)
         print('Age :',self.age)
         print('Place :',self.place)
-        print('Year :',RacingTeamMenmers.year)
+        print('Year :',RacingTeamMembers.year)
+
+    @classmethod                   # classmethod is used for class variable ie year.
+    def changeYear(cls):
+        cls.year=cls.year+1
 
 
-x=RacingTeamMenmers('bmw',1,'Germany')
+    @staticmethod   #static method
+    def display_welcome():
+        print('Welcome')
+
+
+x=RacingTeamMembers('bmw',1,'Germany')
+y=RacingTeamMembers('audi',1,'France')
+
+RacingTeamMembers.display_welcome()
 x.displayAll()
+y.displayAll()
 
+print('--------Afer Update---------------')
 
-RacingTeamMenmers.year=RacingTeamMenmers.year+1
+RacingTeamMembers.changeYear()
 x.addAge()
+y.addAge()
+x.relocate('USA')
+y.relocate('UK')
 x.displayAll()
+y.displayAll()
