b480f670f2bbc33bae374814cfea01721debdb7e
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-13 15:42:54 +0530

    class eg

--- class_sample.py
+++ class_sample.py
@@ -14,3 +14,14 @@ x.print_name()
 y=MySampleClass()
 y.hello('orange')
 y.print_name()
+
+
+class Addition:
+    def twonumadd(num1,num2):
+        return num1+num2
+
+#Addition.twonumadd=staticmethod(Addition.twonumadd)    
+#x=Addition() #if we use self in function then we need to create an instance 
+#sum=x.twonumadd(10,20)
+sum=Addition.twonumadd(10,20)
+print(sum)
