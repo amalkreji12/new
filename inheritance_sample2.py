70eabbcdfcf1616c07b76ce1c25d2c491ddcb40a
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-14 14:00:20 +0530

    inheritance example

--- inheritance_sample2.py
+++ inheritance_sample2.py
@@ -1,8 +1,22 @@
 class BaseClass:
+    def __init__(self):
+        print('Base init')
+
     def set_name(self, name):
         self.name=name
+        print('Base class set_name')
 
-class SubClass(BaseClass):
+
+class SubClass(BaseClass): 
+    def __init__(self):
+        super().__init__()  #super() is used to call from baseclass  (over riding concept)
+        print('Sub class init')
+
+    def set_name(self, name):
+        super().set_name(name)
+        #self.name=name
+        print('Sub class set_name')
+
     def welcome(self):
         print('Welcome')
 
