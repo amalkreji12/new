867f960e7f04f20ab22727b921d8f9669289d7df
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-17 15:57:59 +0530

    GUI sample

--- GUI_sample.py
+++ GUI_sample.py
@@ -1,10 +1,26 @@
 from tkinter import *
 
 window=Tk()
+window.geometry('500x500')
+window.title('Sample')
 
-button=Button(window,text='OK')
+def hello():
+    print('Hello')         #function for what to do when clicking the button
 
-button.pack()
+
+
+button1=Button(window,text='OK',command=hello) #;command is used to call the function
+button2=Button(window,text='OK',command=hello)
+
+label=Label(window,text='Welcome')
+
+button1.grid(row=0,column=0)
+button2.grid(row=0,column=1)
+
+#This code is used when we dont use 'grid' method
+# button1.pack()
+# button2.pack()
+# label.pack()
 
 
 
