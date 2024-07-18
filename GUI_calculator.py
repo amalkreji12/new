1c42c061656e1e9c649ba531968549e763c84c72
Amal K Reji <amalreji45.amal@gmail.com>
2024-07-18 11:23:10 +0530

    gui calculator

--- GUI_calculator.py
+++ GUI_calculator.py
@@ -5,7 +5,7 @@ window.title('Calculator')
 window.geometry('500x500')
 window.configure(bg='light green')
 
-eqn=StringVar() #for taking input as string
+eqn=StringVar() #for taking input as string // used for hold string values
 txt_box=Entry(window,textvariable=eqn)
 txt_box.grid(columnspan=4,ipadx=70)
 
@@ -14,3 +14,6 @@ txt_box.grid(columnspan=4,ipadx=70)
 
 
 window.mainloop()
+
+
+# help : https://www.geeksforgeeks.org/python-simple-gui-calculator-using-tkinter/
